917e46e8308fd270626299e1b31d612c8a20b6f7
Daniel Michel <dmichel@griddynamics.com>
2025-05-09 16:03:58 -0600

    Add a solution to fizzbuzz

---
+++ fizzbuzz.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for i in {1..100}; do
+    RESULT=""
+    if [[ $(($i % 3)) == 0 ]]; then
+        RESULT+="Fizz"
+    fi
+    if [[ $(($i % 5)) == 0 ]]; then
+        RESULT+="Buzz"
+    fi
+    if [[ ${#RESULT} -eq 0 ]]; then
+        echo -n "$i "
+    fi
+    echo "$RESULT"
+done
