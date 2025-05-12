6f775c0a86c04e2c6fb4912dab9f0fc2cfa15b1d
Daniel Michel <dmichel@griddynamics.com>
2025-05-12 11:20:41 -0600

    Change caesar cipher solution
    - Now the solution uses tr, simplifying the code
    - Removed previous input parameter solution

--- caesar_cipher.sh
+++ caesar_cipher.sh
@@ -2,51 +2,21 @@
 
 : ${6?"USAGE: -s [int:shift] -i [string:input_file] -o [string:output_file]"}
 
-# === Function Definitions ===
-char_to_ascii() {
-    printf "%d\n" "'$1"
-}
-
-ascii_to_char() {
-    local ASCII=$1
-    while [[ ASCII -gt 122 ]]; do
-        ASCII=$(($ASCII - 26))
-    done
-    printf "\\x$(printf '%x' "$ASCII")\n"
-}
-
-# === Input parameters ===
-while [[ "$#" -gt 0 ]]; do
-    case "$1" in
-    -s)
-        SHIFT="$2"
-        shift 2
-        ;;
-    -i)
-        INPUT="$2"
-        shift 2
-        ;;
-    -o)
-        OUTPUT="$2"
-        shift 2
-        ;;
-    *)
-        echo "Unknown option: $1"
-        exit 1
-        ;;
-    esac
-done
-
+SHIFT="$2"
+INPUT="$4"
+OUTPUT="$6"
 
 # === Main Script ===
-echo "" > $OUTPUT
-while IFS= read -r -n1 char; do
-    if [[ "$char" =~ [A-Za-z] ]]; then
-        char=$(echo "$char" | tr 'A-Z' 'a-z')
-        char=$( char_to_ascii $char )
-        char=$(( $char + $SHIFT ))
-        char=$( ascii_to_char $char )
-    fi
-    printf "$char" >> $OUTPUT
-    
-done < $INPUT
+SHIFT=$(($SHIFT % 26))
+
+# Build rotated alphabets dynamically
+ORIG=abcdefghijklmnopqrstuvwxyz
+ROTATED=${ORIG:$SHIFT}${ORIG:0:$SHIFT}
+
+# Lowercase contents of input file
+PLAINTEXT=$(echo `cat "$INPUT"` | tr 'A-Z' 'a-z')
+
+# Encrypt contents using tr
+CIPHERTEXT=$(echo $PLAINTEXT | tr "$ORIG" "$ROTATED")
+
+echo -n $CIPHERTEXT > $OUTPUT
