14602a838c85293881432b67e0eac8584be5bd5f
Daniel Michel <dmichel@griddynamics.com>
2025-05-13 10:23:07 -0600

    Fixed problems using shellcheck

--- caesar_cipher.sh
+++ caesar_cipher.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-: ${6?"USAGE: -s [int:shift] -i [string:input_file] -o [string:output_file]"}
+: "${6?"USAGE: -s [int:shift] -i [string:input_file] -o [string:output_file]"}"
 
 SHIFT="$2"
 INPUT="$4"
 OUTPUT="$6"
 
 # === Main Script ===
-SHIFT=$(($SHIFT % 26))
+SHIFT=$((SHIFT % 26))
 
 # Build rotated alphabets dynamically
 ORIG=abcdefghijklmnopqrstuvwxyz
 ROTATED=${ORIG:$SHIFT}${ORIG:0:$SHIFT}
 
 # Lowercase contents of input file
-PLAINTEXT=$(echo `cat "$INPUT"` | tr 'A-Z' 'a-z')
+PLAINTEXT=$( < "$INPUT" tr '[:upper:]' '[:lower:]')
 
 # Encrypt contents using tr
-CIPHERTEXT=$(echo $PLAINTEXT | tr "$ORIG" "$ROTATED")
+CIPHERTEXT=$(echo "$PLAINTEXT" | tr "$ORIG" "$ROTATED")
 
-echo -n $CIPHERTEXT > $OUTPUT
+echo -n "$CIPHERTEXT" > "$OUTPUT"
