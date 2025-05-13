4963293e7a2f1777e6ce3d06965d0c0995093aa1
Daniel Michel <dmichel@griddynamics.com>
2025-05-13 08:59:49 -0600

    Add solution to report.sh

---
+++ report.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+: ${1?"USAGE: [string:output_file]"}
+
+OUTPUT="$1"
+
+echo "Date & Time: $(date)" > $OUTPUT
+echo "Username: $USER" >> $OUTPUT
+echo "Hostname: $(hostname)" >> $OUTPUT
+echo "External IP Address: $(curl -s icanhazip.com)" >> $OUTPUT
+echo "System Uptime: $(uptime)" >> $OUTPUT
+echo "Disk Used: $(df -h | grep '/$' | awk '{print $3}')" >> $OUTPUT
+echo "Disk Free: $(df -h | grep '/$' | awk '{print $4}')" >> $OUTPUT
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Linux Distribution: $(lsb_release -d | awk -F'\t' '{print $2}')" >> $OUTPUT
+    echo "Internal IP Address: $(hostname -I | awk '{print $1}')" >> $OUTPUT
+    echo "RAM Total in GB: $(free -h | grep "Mem:" | awk '{print $2}')" >> $OUTPUT
+    echo "RAM Free in GB: $(free -h | grep "Mem:" | awk '{print $4}')" >> $OUTPUT
+    echo "CPU Cores: $(nproc)" >> $OUTPUT
+    echo "CPU Frequency in GHz: $(cat /proc/cpuinfo | grep "MHz" | head -n 1 | awk '{print $4/1000}') GHz" >> $OUTPUT
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "macOS Version: $(sw_vers -productName) $(sw_vers -productVersion)" >> $OUTPUT
+    echo "Internal IP Address: $(ipconfig getifaddr en0)" >> $OUTPUT
+    echo "RAM Total in GB: $(sysctl -n hw.memsize | awk '{print $1/1024/1024/1024}') GB" >> $OUTPUT
+    echo "RAM Free in GB: $(vm_stat | grep "Pages free" | awk '{print $3/1024/1024/1024*4096}') GB" >> $OUTPUT
+    echo "CPU Cores: $(sysctl -n hw.physicalcpu)" >> $OUTPUT
+    echo "CPU Frequency in GHz: $(sysctl -n hw.cpufrequency | awk '{print $1/1000000000}') GHz" >> $OUTPUT
+fi
