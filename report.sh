d9c3040584ae7ae7bb9ea32bdb56eeb01be2d0f3
Daniel Michel <dmichel@griddynamics.com>
2025-05-13 10:43:24 -0600

    Group commands and redirect the group

--- report.sh
+++ report.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
-: ${1?"USAGE: [string:output_file]"}
+: "${1?"USAGE: [string:output_file]"}"
 
 OUTPUT="$1"
 
-echo "Date & Time: $(date)" > $OUTPUT
-echo "Username: $USER" >> $OUTPUT
-echo "Hostname: $(hostname)" >> $OUTPUT
-echo "External IP Address: $(curl -s icanhazip.com)" >> $OUTPUT
-echo "System Uptime: $(uptime)" >> $OUTPUT
-echo "Disk Used: $(df -h | grep '/$' | awk '{print $3}')" >> $OUTPUT
-echo "Disk Free: $(df -h | grep '/$' | awk '{print $4}')" >> $OUTPUT
+echo "" > "$OUTPUT"
+{
+    echo "Date & Time: $(date)"
+    echo "Username: $USER"
+    echo "Hostname: $(hostname)"
+    echo "External IP Address: $(curl -s icanhazip.com)"
+    echo "System Uptime: $(uptime)"
+    echo "Disk Used: $(df -h | grep '/$' | awk '{print $3}')"
+    echo "Disk Free: $(df -h | grep '/$' | awk '{print $4}')"
+} >> "$OUTPUT"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "Linux Distribution: $(lsb_release -d | awk -F'\t' '{print $2}')" >> $OUTPUT
-    echo "Internal IP Address: $(hostname -I | awk '{print $1}')" >> $OUTPUT
-    echo "RAM Total in GB: $(free -h | grep "Mem:" | awk '{print $2}')" >> $OUTPUT
-    echo "RAM Free in GB: $(free -h | grep "Mem:" | awk '{print $4}')" >> $OUTPUT
-    echo "CPU Cores: $(nproc)" >> $OUTPUT
-    echo "CPU Frequency in GHz: $(cat /proc/cpuinfo | grep "MHz" | head -n 1 | awk '{print $4/1000}') GHz" >> $OUTPUT
+    {
+        echo "Linux Distribution: $(lsb_release -d | awk -F'\t' '{print $2}')"
+        echo "Internal IP Address: $(hostname -I | awk '{print $1}')"
+        echo "RAM Total in GB: $(free -h | grep "Mem:" | awk '{print $2}')"
+        echo "RAM Free in GB: $(free -h | grep "Mem:" | awk '{print $4}')"
+        echo "CPU Cores: $(nproc)"
+        echo "CPU Frequency in GHz: $( < /proc/cpuinfo grep "MHz" | head -n 1 | awk '{print $4/1000}') GHz"
+    } >> "$OUTPUT"
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "macOS Version: $(sw_vers -productName) $(sw_vers -productVersion)" >> $OUTPUT
-    echo "Internal IP Address: $(ipconfig getifaddr en0)" >> $OUTPUT
-    echo "RAM Total in GB: $(sysctl -n hw.memsize | awk '{print $1/1024/1024/1024}') GB" >> $OUTPUT
-    echo "RAM Free in GB: $(vm_stat | grep "Pages free" | awk '{print $3/1024/1024/1024*4096}') GB" >> $OUTPUT
-    echo "CPU Cores: $(sysctl -n hw.physicalcpu)" >> $OUTPUT
-    echo "CPU Frequency in GHz: $(sysctl -n hw.cpufrequency | awk '{print $1/1000000000}') GHz" >> $OUTPUT
+    {
+        echo "macOS Version: $(sw_vers -productName) $(sw_vers -productVersion)"
+        echo "Internal IP Address: $(ipconfig getifaddr en0)"
+        echo "RAM Total in GB: $(sysctl -n hw.memsize | awk '{print $1/1024/1024/1024}') GB"
+        echo "RAM Free in GB: $(vm_stat | grep "Pages free" | awk '{print $3/1024/1024/1024*4096}') GB"
+        echo "CPU Cores: $(sysctl -n hw.physicalcpu)"
+        echo "CPU Frequency in GHz: $(sysctl -n hw.cpufrequency | awk '{print $1/1000000000}') GHz"
+    } >> "$OUTPUT"
 fi
