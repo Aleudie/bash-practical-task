dda93b12a930d84b42c3e7417bf4e5294b1fe62e
Daniel Michel <dmichel@griddynamics.com>
2025-05-13 10:52:55 -0600

    Fixed problems using shellcheck

--- strings.sh
+++ strings.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-: ${5?"USAGE: -i [string:input_file] -o [string:output_file] [-v | -s | -r | -l | -u]"}
+: "${5?"USAGE: -i [string:input_file] -o [string:output_file] [-v | -s | -r | -l | -u]"}"
 
 if [[ $5 == "-s" ]]; then
-    : ${7?"USAGE: -i [string:input_file] -o [string:output_file] [-v | -s <A_WORD> <B_WORD> | -r | -l | -u]"}  
+    : "${7?"USAGE: -i [string:input_file] -o [string:output_file] [-v | -s <A_WORD> <B_WORD> | -r | -l | -u]"}"
 fi
 
 while [[ "$#" -gt 0 ]]; do
@@ -18,32 +18,32 @@ while [[ "$#" -gt 0 ]]; do
         ;;
     -v)
         # Replaces $INPUT lowercase characters with uppercase and vise versa
-        REPLACED=$(echo `cat "$INPUT"` | tr 'a-zA-Z' 'A-Za-z')
-        echo -n $REPLACED > $OUTPUT
+        REPLACED=$( < "$INPUT" tr 'a-zA-Z' 'A-Za-z')
+        echo -n "$REPLACED" > "$OUTPUT"
         shift
         ;;
     -s)
         # Substitutes $INPUT <A_WORD> with <B_WORD>
         A_WORD="$2"
         B_WORD="$3"
-        SUBSTITUTED=$(echo `cat "$INPUT"` | sed "s/$A_WORD/$B_WORD/g")
-        echo -n $SUBSTITUTED > $OUTPUT
+        SUBSTITUTED=$( < "$INPUT" sed "s/$A_WORD/$B_WORD/g")
+        echo -n "$SUBSTITUTED" > "$OUTPUT"
         shift 3
         ;;
     -r)
         # Reverses $INPUT lines
         REVERSED=$(tac "$INPUT")
-        echo "$REVERSED" > $OUTPUT
+        echo "$REVERSED" > "$OUTPUT"
         shift
         ;;
     -l)
-        LOWERCASE=$(echo `cat "$INPUT"` | tr 'A-Z' 'a-z')
-        echo -n $LOWERCASE > $OUTPUT
+        LOWERCASE=$( < "$INPUT" tr '[:upper:]' '[:lower:]')
+        echo -n "$LOWERCASE" > "$OUTPUT"
         shift
         ;;
     -u)
-        UPPERCASE=$(echo `cat "$INPUT"` | tr 'a-z' 'A-Z')
-        echo -n $UPPERCASE > $OUTPUT
+        UPPERCASE=$( < "$INPUT" tr '[:lower:]' '[:upper:]')
+        echo -n "$UPPERCASE" > "$OUTPUT"
         shift
         ;;
     *)
