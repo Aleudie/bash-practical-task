64721da36857827de21cdf5745740b7b49ddb1a3
Daniel Michel <dmichel@griddynamics.com>
2025-05-13 10:24:43 -0600

    Fixed problems using shellcheck

--- fibonacci.sh
+++ fibonacci.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-: ${1?"n: $1 integer expected"}
+: "${1?"n: $1 integer expected"}"
 
 N=$1
 function fib {
-    if [ $1 -lt 2 ]; then
-        echo $1
+    local a
+    local b
+    if [ "$1" -lt 2 ]; then
+        echo "$1"
     else
-        local a=$(fib $(($1 - 1)))
-        local b=$(fib $(($1 - 2)))
-        echo $(($a + $b))
+        a=$(fib $(($1 - 1)))
+        b=$(fib $(($1 - 2)))
+        echo $((a + b))
     fi
 }
 
-fib $N
+fib "$N"
